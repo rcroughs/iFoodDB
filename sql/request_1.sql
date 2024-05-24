9ab26c90c6179312fc02a43e95e3945709dc1b29
Chris Eid <chris.eid@ulb.be>
2024-05-24 23:04:27 +0200

    requete 1

--- sql/request_1.sql
+++ sql/request_1.sql
@@ -1,1 +1,6 @@
 -- Les restaurants ayant un avis moyen de plus de 3
+SELECT r.name, AVG(c.rating) AS average_rating
+FROM Restaurant r
+JOIN Comment c ON r.id = c.restaurant_id
+GROUP BY r.name
+HAVING AVG(c.rating) >= 3;
