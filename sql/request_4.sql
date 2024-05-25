798eba21d6d2453b57ad2b9babbbcc0353c80b7a
rcroughs <romain.croughs@gmail.com>
2024-05-25 19:51:05 +0200

    fixing request 4
    The issue was that we were taking ids instead name and precising
    generally

--- sql/request_4.sql
+++ sql/request_4.sql
@@ -2,32 +2,33 @@
 -- Step 1: Identify dishes served in Asian restaurants
 -- Étape 1: Identifier les plats servis dans les restaurants asiatiques
 WITH AsianRestaurantDishes AS (
-    SELECT p.ID AS dish_id
+    SELECT p.ID AS dish_id, p.NAME AS dish_name
     FROM restaurants r
     JOIN plats p ON r.MENU = p.MENU
-    WHERE r.FOOD_TYPE = 'Asian'
+    WHERE r.FOOD_TYPE = 'asiatique'
 ),
 
 -- Étape 2: Compter combien de restaurants asiatiques servent chaque plat
 DishCountInAsianRestaurants AS (
-    SELECT dish_id, COUNT(*) AS asian_restaurant_count
+    SELECT dish_name, COUNT(*) AS asian_restaurant_count
     FROM AsianRestaurantDishes
-    GROUP BY dish_id
+    GROUP BY dish_name
 ),
 
 -- Étape 3: Identifier les plats servis dans les restaurants non asiatiques
 NonAsianRestaurantDishes AS (
-    SELECT r.ID AS restaurant_id, p.ID AS dish_id
+    SELECT r.ID AS restaurant_id, p.NAME AS dish_name
     FROM restaurants r
     JOIN plats p ON r.MENU = p.MENU
-    WHERE r.FOOD_TYPE != 'Asian'
+    WHERE r.FOOD_TYPE != 'asiatique'
 ),
 
 -- Étape 4: Compter le nombre de plats typiquement servis dans les restaurants asiatiques qui sont servis dans chaque restaurant non asiatique
 PopularDishesInNonAsianRestaurants AS (
     SELECT nard.restaurant_id, COUNT(*) AS popular_dish_count
     FROM NonAsianRestaurantDishes nard
-    JOIN DishCountInAsianRestaurants dar ON nard.dish_id = dar.dish_id
+    JOIN DishCountInAsianRestaurants dar ON nard.dish_name = dar.dish_name
+    WHERE dar.asian_restaurant_count >= 2
     GROUP BY nard.restaurant_id
 )
 
@@ -35,5 +36,5 @@ PopularDishesInNonAsianRestaurants AS (
 SELECT r.NAME, r.FOOD_TYPE, pdr.popular_dish_count
 FROM PopularDishesInNonAsianRestaurants pdr
 JOIN restaurants r ON pdr.restaurant_id = r.ID
-ORDER BY pdr.popular_dish_count DESC
-LIMIT 1;
+ORDER BY pdr.popular_dish_count DESC LIMIT 1;
+
