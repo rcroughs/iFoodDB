e7132ff189929b1b4156c8f5dc3dcf22b1318a6f
Chris Eid <chris.eid@ulb.be>
2024-05-25 16:20:56 +0200

    starting query 4

--- sql/request_4.sql
+++ sql/request_4.sql
@@ -1,1 +1,7 @@
 -- Le restaurant non-asiatique proposant le plus de plats qui sont généralement proposés dans des restaurant asiatiques
+WITH AsianRestaurantDishes AS(
+    SELECT rd.dish_id
+    FROM Restaurants r
+    JOIN RestaurantDishes rd ON r.restaurant_id = rd.restaurant_id
+    WHERE r.cuisine_type = 'Asian'
+),
